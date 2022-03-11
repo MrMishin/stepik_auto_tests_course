a8bb87be0c7bc24c2059f23d3630f76d9aa5f929
AlekseyMishin <alekseimishin@mail.ru>
2022-03-11 10:38:30 +0500

    Py script for language task git status

--- Section#3_language_test/test_items.py
+++ Section#3_language_test/test_items.py
@@ -1,6 +1,10 @@
-link = "http://selenium1py.pythonanywhere.com/"
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+
+    browser.implicitly_wait(30)
+    button = browser.find_element_by_css_selector("button.btn.btn-add-to-basket")
+    assert button != None, "Bucket button is not found!"
